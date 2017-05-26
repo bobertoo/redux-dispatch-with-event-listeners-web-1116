03d8254bbc9f78d77e6d2a0ada515b80cee3345f
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-26 10:16:45 -0400

    counter button working

--- reducer.js
+++ reducer.js
@@ -1,9 +1,28 @@
-let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+let state,
+  button = document.getElementById('button'),
+  container = document.getElementById('container');
+
+
+function changeState(state = { count: 0 }, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+    default:
+      return state;
   }
+}
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render(){
+  container.textContent = state.count;
+}
+
+button.addEventListener('click', function() {
+  dispatch({ type: 'INCREASE_COUNT' });
+});
+
+dispatch({ type: '@@INIT' });
